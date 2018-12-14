f750bc7b34e611c802f007e7f5ed766370b0f46b
ISABELLE TERWAGNE <isabelleterwagne@macbook-air-de-isabelle.local>
2018-12-14 21:40:54 +0100

    Initial Commit

---
+++ P3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  P3
+//
+//  Created by ISABELLE Terwagne on 14/12/2018.
+//  Copyright © 2018 ISABELLE Terwagne. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
